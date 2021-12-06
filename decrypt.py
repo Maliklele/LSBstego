91932384ccf08638862268d3a453c9dc2516f941
Maliklele <truxton3@gmail.com>
2021-12-06 23:56:43 +0300

    first commit

---
+++ decrypt.py
@@ -0,0 +1,59 @@
+import PIL as pl
+from PIL import Image
+
+
+
+img = Image.open("images/processed.png")
+print("Image mode", img.mode)
+print("Image Size",img.size[0],"x",img.size[1],"=",img.size[0]*img.size[1])
+
+
+#creating array that holds the hidden bits
+bina = []
+
+for x in range(img.size[0]):
+    for y in range(img.size[1]):
+        _rgb = img.getpixel((x,y))
+        _r_bina = format(_rgb[0],'08b')[6:8]
+        _g_bina = format(_rgb[1],'08b')[6:8]
+        _b_bina = format(_rgb[2],'08b')[6:8]
+        bina.append(_r_bina)
+        bina.append(_g_bina)
+        bina.append(_b_bina)
+
+for x in range(3):
+    if(len(bina)%4!=0):
+        bina.append('00')
+    else:
+        break
+
+#print(bina)
+characters = []
+
+
+def convert(s):
+  
+    # initialization of string to ""
+    new = ""
+  
+    # traverse in the string 
+    for x in s:
+        new += x 
+  
+    # return string 
+    return new
+      
+
+
+x=0
+while(x<len(bina)):
+    if(int(bina[x]+bina[x+1]+bina[x+2]+bina[x+3],2)>127):
+        break
+    #print(bina[x]+bina[x+1]+bina[x+2]+bina[x+3])
+    characters.append(chr(int(bina[x]+bina[x+1]+bina[x+2]+bina[x+3],2)))
+    x=x+4
+
+text = convert(characters)
+print(text)
+with open("decrypted.txt", "w") as f:
+    f.writelines(str(text))
---
+++ encrypt.py
@@ -0,0 +1,73 @@
+import PIL as pl
+from PIL import Image
+
+
+_TEXT_PATH = "text.txt"
+_IMAGE_PATH = "images\original.png"
+
+
+
+#Reeading text file and returning array with bits of
+def readFile(path):
+    fileR = open("text.txt", 'r')
+    msg = fileR.read()
+    binaArr = []
+    for element in msg:  
+        binaArr.append(format(ord(element), '08b'))
+    splitBinaArray = []
+    #print(binaArr)
+    for x in binaArr:
+        splitBinaArray.append(x[0:2])
+        splitBinaArray.append(x[2:4])
+        splitBinaArray.append(x[4:6])
+        splitBinaArray.append(x[6:8])
+    
+    for x in range(4):
+        if (len(splitBinaArray)%3!=0):
+            splitBinaArray.append('00')
+        else:
+            break
+
+    return splitBinaArray
+
+
+    
+    
+
+bina = readFile(_TEXT_PATH)
+#print(bina)
+
+originalImage = Image.open(_IMAGE_PATH)
+processedImage = originalImage.copy()
+
+
+i=0
+halt=False
+for x in range(processedImage.size[0]):
+    for y in range(processedImage.size[1]):
+        if(i<len(bina)):
+            
+            _rgb = processedImage.getpixel((x,y))
+            _r=int(format(_rgb[0],'08b')[0:6]+bina[i],2)
+            _g=int(format(_rgb[1],'08b')[0:6]+bina[i+1],2)
+            _b=int(format(_rgb[2],'08b')[0:6]+bina[i+2],2)
+            
+            processedImage.putpixel((x,y),(_r,_g,_b))
+            print(processedImage.getpixel((x,y)))
+            i=i+3
+        else:
+            halt=True
+            break
+    if(halt):
+        break
+
+print("Text size = ",len(bina))
+
+print("Image mode", processedImage.mode)
+print("Image Size",processedImage.size[0],"x",processedImage.size[1],"=",processedImage.size[0]*originalImage.size[1])
+
+
+print("L:",len(bina))
+processedImage.save("images\processed.png",format="png",quality=100)
+
+
